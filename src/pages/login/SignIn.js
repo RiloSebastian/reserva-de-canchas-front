0f913202b2edff3cf893d3c0d899d4feb0dcf4d9
martinianodambrosio <martinianodambrosio@outlook.com>
2022-05-01 01:49:37 -0300

    refactor router link

--- src/pages/login/SignIn.js
+++ src/pages/login/SignIn.js
@@ -5,7 +5,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
+import { Link } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
@@ -119,10 +119,10 @@ const SignIn = (props) => {
                 id="password"
                 autoComplete="current-password"
               />
-              <FormControlLabel
+              {/*<FormControlLabel
                 control={<Checkbox value="remember" color="primary" />}
                 label="Recordarme"
-              />
+          />*/}
               <Button
                 type="submit"
                 fullWidth
@@ -132,13 +132,13 @@ const SignIn = (props) => {
                 {"Iniciar Sesion"}
               </Button>
               <Grid container>
-                <Grid item xs>
-                  <Link href="/forgot-pass" variant="body2">
+                {/*<Grid item xs>
+                  <Link to="/forgot-pass" variant="body2">
                     Olvidaste la Contraseña?
                   </Link>
-                </Grid>
+        </Grid>*/}
                 <Grid item>
-                  <Link href="/signup" variant="body2">
+                  <Link to="/signup" variant="body2">
                     {"Aun no tienes una Cuenta? Registrarse"}
                   </Link>
                 </Grid>
--- src/pages/signup/SignUp.js
+++ src/pages/signup/SignUp.js
@@ -3,9 +3,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
+import { Link } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
@@ -244,7 +242,7 @@ const SignUp = () => {
               </Button>
               <Grid container justifyContent="flex-end">
                 <Grid item>
-                  <Link href="/login" variant="body2">
+                  <Link to="/login" variant="body2">
                     Ya tenes una Cuenta? Inicia Sesion
                   </Link>
                 </Grid>
